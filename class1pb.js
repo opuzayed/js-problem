7cf6a57e5460393b4f1b884555e5499efea3a6b5
opuzayed <zayedopu4933@gmail.com>
2022-08-01 12:22:01 +0600

    first problem solved

---
+++ class1pb.js
@@ -0,0 +1,6 @@
+var momGave = 1000;
+var orangePrice = 400;
+var applePrice = 300;
+var totalCost = orangePrice + applePrice;
+var remainingMoney = momGave - totalCost;
+console.log(remainingMoney);
